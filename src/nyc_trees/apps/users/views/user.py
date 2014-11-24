05f422c5c6d077e7e428782a8da6be757d81d52a
Rick Mohr <rmohr@azavea.com>
2014-11-24 14:24:34 -0500

    User profile settings form
    * Add User model form
    * Add `settings.html` template using the form
    * Add `privacy_controls.html` partial (which will also be used in popup from profile page)
    * Add views
    * Strip whitespace from first/last name in `User.clean`
    
    In a future PR:
    * Add Bootstrap so tab panels will work
    * Add JS to enable/disable privacy controls and set hidden fields

--- src/nyc_trees/apps/users/views/user.py
+++ src/nyc_trees/apps/users/views/user.py
@@ -5,12 +5,11 @@ from __future__ import division
 
 from django.core.urlresolvers import reverse
 from django.http import Http404, HttpResponseRedirect
-from django.shortcuts import get_object_or_404
-
-from django_tinsel.decorators import render_template
-from django_tinsel.utils import decorate as do
+from django.shortcuts import get_object_or_404, render_to_response
+from django.template import RequestContext
 
 from apps.core.models import User
+from apps.users.forms import ProfileSettingsForm
 
 
 def user_detail_redirect(request):
@@ -18,7 +17,7 @@ def user_detail_redirect(request):
         reverse('user_detail', kwargs={'username': request.user.username}))
 
 
-def user_profile_context(request, username):
+def user_detail(request, username):
     user = get_object_or_404(User, username=username)
     its_me = (user.id == request.user.id)
 
@@ -26,7 +25,11 @@ def user_profile_context(request, username):
         # Private profile acts like a missing page to others
         raise Http404()
 
-    return {
+    return _render_user_profile(request, user, its_me)
+
+
+def _render_user_profile(request, user, its_me):
+    context = {
         'user': user,
         'viewing_own_profile': its_me,
         'show_username': ((its_me or user.real_name_is_public) and
@@ -37,6 +40,52 @@ def user_profile_context(request, username):
         'show_individual_mapper': (user.individual_mapper and
                                    (its_me or user.profile_is_public))
     }
+    return render_to_response('users/profile.html', context,
+                              RequestContext(request))
+
+
+def profile_settings(request):
+    form = ProfileSettingsForm(instance=request.user, label_suffix='')
+    form.fields['opt_in_events_info'].label = 'Yes'
+    form.fields['opt_in_stewardship_info'].label = 'Yes'
+    context = {
+        'form': form,
+        'privacy_categories': _get_privacy_categories(form),
+        'username': request.user.username,
+    }
+    return render_to_response('users/settings.html', context,
+                              RequestContext(request))
+
+
+def _get_privacy_categories(form):
+    user = form.instance
+    return [
+        {
+            'title': 'Profile',
+            'is_public': user.profile_is_public,
+            'form_field': form['profile_is_public']
+        }, {
+            'title': 'Groups',
+            'is_public': user.group_follows_are_public,
+            'form_field': form['group_follows_are_public']
+        }, {
+            'title': 'Contributions',
+            'is_public': user.contributions_are_public,
+            'form_field': form['contributions_are_public']
+        }, {
+            'title': 'Achievements',
+            'is_public': user.achievements_are_public,
+            'form_field': form['achievements_are_public']
+        }
+    ]
+
+
+def update_profile_settings(request):
+    form = ProfileSettingsForm(request.POST, instance=request.user)
+    # It's not possible to create invalid data with this form,
+    # so don't check form.is_valid()
+    form.save()
+    return _render_user_profile(request, request.user, its_me=True)
 
 
 def update_user(request, username):
@@ -62,8 +111,3 @@ def start_map_for_reservation_job(request, username):
 def start_map_for_tool_depots_job(request, username):
     # TODO: implement
     pass
-
-
-user_detail = do(
-    render_template('users/profile.html'),
-    user_profile_context)
