870b73e7e8d2556fda2d9aef9f3dc416212cd313
Rick Mohr <rmohr@azavea.com>
2014-12-22 14:24:11 -0500

    User privacy controls
    Back end:
    * Separate `PrivacySettingsForm` from `ProfileSettingsForm`
    * User settings page: stay on page after save or cancel (per JeffF)
    * Postback to user profile updates privacy settings
    
    Front end:
    * Add bootstrap modal to show privacy settings on profile page
    * Module `privacySettings.js` controls toggling and display of privacy settings
    * Used in both user profile and settings pages, each with their own save and cancel buttons
    
    Fixes #174

---
+++ src/nyc_trees/js/src/userProfile.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var $ = require('jquery'),
+    privacySettings = require('./privacySettings.js'),
+
+    dom = {
+        privacyForm: '#privacy-form',
+        cancelButton: '.js-cancel'
+    };
+
+$(dom.privacyForm).on('submit', function () {
+    privacySettings.prepareForSave();
+    // Note: postback rather than ajax update because changing privacy settings
+    // may affect how the user profile template is rendered.
+});
+
+$(dom.cancelButton).on('click', function () {
+    privacySettings.revert();
+});
