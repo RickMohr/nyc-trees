3d4fd152bf8d3f4330d3c9a7d2226ac8c28a2863
RickMohr <rmohr@azavea.com>
2015-02-03 12:52:32 -0500

    wip

--- src/nyc_trees/js/src/utils.js
+++ src/nyc_trees/js/src/utils.js
@@ -5,7 +5,7 @@ module.exports = {
     copyToClipboard: function (text) {
         var isMac = navigator.platform.toUpperCase().indexOf('MAC') !== -1,
             shortcut = isMac ? 'Cmd+C' : 'Ctrl+C',
-            prompt = 'Type <' + shortcut + '>, <Enter> to copy to clipboard.';
+            prompt = 'Hit <' + shortcut + '>, <Enter> to copy to clipboard.';
         window.prompt(prompt, text)
     }
 
