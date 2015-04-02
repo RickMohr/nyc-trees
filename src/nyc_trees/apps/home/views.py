ad554c0946a069c1424c3fd2d6cdd2895a52be87
Rick Mohr <rmohr@azavea.com>
2015-04-02 13:36:14 -0400

    wip

--- src/nyc_trees/apps/home/views.py
+++ src/nyc_trees/apps/home/views.py
@@ -17,35 +17,15 @@ from apps.users import user_profile_context
 
 
 def home_page(request):
-    # In order to show the tree count in a "ticker" we need to break it up
-    # into digits and pad it with zeroes.
-    tree_count = get_total_tree_count()
-    trees_digits = [digit for digit in "{:07d}".format(tree_count)]
-
     if request.user.is_authenticated():
-        context = user_profile_context(request.user, True)
+        context = user_profile_context(request.user)
         context['training_steps'] = training_summary.get_context(request.user)
-        context['tree_count'] = trees_digits
+        context['counts_all'] = _global_counts()
+        context['counts_week'] = _global_counts()
     else:
-        blocks_total = Blockface.objects.all().count()
-        blocks_mapped = Survey.objects.all().distinct('blockface_id').count()
-        if blocks_mapped > 0:
-            blocks_percent = "{:.0%}".format(
-                float(blocks_mapped) / float(blocks_total))
-        else:
-            blocks_percent = "0%"
-
-        user_count = User.objects.filter(is_active=True).count()
-
         context = {
             'user': request.user,
-            'counts': {
-                'species': get_total_species_count(),
-                'tree': trees_digits,
-                'block': blocks_mapped,
-                'block_percent': blocks_percent,
-                'user': user_count,
-            }
+            'counts': _global_counts()
         }
 
     immediate_events_list = (immediate_events
@@ -54,6 +34,33 @@ def home_page(request):
 
     context.update({'immediate_events': immediate_events_list})
     return context
+
+
+def _global_counts():
+    blocks_total = Blockface.objects.all().count()
+    blocks_mapped = Survey.objects.all().distinct('blockface_id').count()
+    if blocks_mapped > 0:
+        blocks_percent = "{:.0%}".format(
+            float(blocks_mapped) / float(blocks_total))
+    else:
+        blocks_percent = "0%"
+
+    user_count = User.objects.filter(is_active=True).count()
+
+    # In order to show the tree count in a "ticker" we need to break it up
+    # into digits and pad it with zeroes.
+    tree_count = get_total_tree_count()
+    trees_digits = [digit for digit in "{:07d}".format(tree_count)]
+
+    global_counts = {
+        'species': get_total_species_count(),
+        'tree': trees_digits,
+        'block': blocks_mapped,
+        'block_percent': blocks_percent,
+        'user': user_count,
+        'event': -999
+        }
+    return global_counts
 
 
 def retrieve_job_status(request):
